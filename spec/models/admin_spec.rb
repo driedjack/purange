605713a92bb38c74456902f17fb00e229480d77b
driedjack <driedjack@gmail.com>
2019-07-18 23:09:29 +0700

    xác minh các thuộc tính của admin

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_secure_password }
+  %i(name password).each { |attr| it { should validate_presence_of attr } }
+  it { should validate_length_of(:name).is_at_least(3).is_at_most(25) }
+  it { should allow_value('dried_jack27').for :name }
+  it { should_not allow_value('123@#').for :name }
 end
