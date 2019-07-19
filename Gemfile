6d233422e275645e6e58d8c25dd72866a7ebadfb
driedjack@gmail.com <driedjack@gmail.com>
2019-07-19 17:31:23 +0700

    bỏ gem đã lỗi thời

--- Gemfile
+++ Gemfile
@@ -57,7 +57,6 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'webdrivers'
   # Easy installation and use of chromedriver to run system tests with Chrome
-  gem 'chromedriver-helper'
   gem 'shoulda-matchers'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,4 @@ ActiveRecord::Schema.define(version: 2019_07_18_154335) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email", null: false
-    t.string "uid"
-    t.string "avatar_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
