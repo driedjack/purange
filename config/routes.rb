f93bf86605a6c11931a7ffceb3247b133a0b6d89
driedjack <driedjack@gmail.com>
2019-07-22 22:32:25 +0700

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     get :login, to: 'sessions#new'
     post :login, to: 'sessions#create'
     delete :sign_out, to: 'sessions#destroy'
+
+    resources :books
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
